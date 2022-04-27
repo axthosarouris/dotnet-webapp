bcad393bea18d808eb7beadaae0e5be29b5963bf
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-04-27 23:14:52 +0200

    #9: configure Swagger to appear in the project root

--- PracticeWebApp/Program.cs
+++ PracticeWebApp/Program.cs
@@ -1,10 +1,14 @@
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -12,9 +16,22 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    ConfigureSwagger();
 }
+
+void ConfigureSwagger()
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => { SwaggerPageAppearsAtProjectRoot(options); });
+    
+    void SwaggerPageAppearsAtProjectRoot(SwaggerUIOptions swaggerUiOptions)
+    {
+        swaggerUiOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        swaggerUiOptions.RoutePrefix = string.Empty;
+    }
+}
+
+
 
 app.UseHttpsRedirection();
 
