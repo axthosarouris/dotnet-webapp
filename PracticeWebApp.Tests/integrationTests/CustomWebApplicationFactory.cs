975c3ec4e0047d035a2e23570ca44b2cbcda71bd
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-05-02 23:46:54 +0200

    First integration test

---
+++ PracticeWebApp.Tests/integrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace PracticeWebApp.Tests.integrationTests;
+
+ public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup: class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services => { });
+        
+    }
+    
+    
+}
